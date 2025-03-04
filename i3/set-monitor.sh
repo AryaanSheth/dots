7fa34b010b46dc02c7e095f41ceea8b2a304c929
AryaanSheth <avsheth03@gmail.com>
2025-03-04 09:36:47 -0500

    fix: update nvim and monitor init

--- i3/set-monitor.sh
+++ i3/set-monitor.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#  Wait for X to be ready before running xrandr
+# Wait for X to be ready before running xrandr
 sleep 2
 
-# Set the 1440p monitor (HDMI-0) as the primary on the left
-xrandr --output HDMI-0 --mode 2560x1440 --primary --rate 60
+# Set the 1080p monitor (DP-2) as the primary on the right
+xrandr --output DP-2 --mode 1920x1080 --primary --rate 144 --right-of HDMI-1
 
-# Set the 1080p monitor (DP-2) on the right of the primary monitor (HDMI-0)
-xrandr --output DP-2 --mode 1920x1080 --rate 144 --right-of HDMI-0
+# Set the 1440p monitor (HDMI-1) on the left
+xrandr --output HDMI-1 --mode 2560x1440 --rate 60
